a014662fc6728980ef00a191faf801061a74b657
umpei1109 <jpdgmatpm674@gmail.com>
2022-05-23 21:22:31 +0900

    umpei220523

---
+++ Unity_Karaoke/Assets/04_Script/90_Lesson/UmpeiPractice.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UmpeiPractice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("•¶Žš");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
