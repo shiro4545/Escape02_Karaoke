7a05717f2d07bea0c92539de486964ccb63db56a
shiro4545 <a3062sm@gmail.com>
2022-07-25 23:56:38 +0900

    iOS1.4.0

--- Unity_Karaoke/Assets/04_Script/02_UI/OtherAppManager.cs
+++ Unity_Karaoke/Assets/04_Script/02_UI/OtherAppManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class OtherAppManager : MonoBehaviour
 {
+    //病室ボタン
+    public GameObject BtnByoshitsu;
     //トイレボタン
     public GameObject BtnToilet;
 
@@ -26,19 +28,42 @@ public class OtherAppManager : MonoBehaviour
     //</summary>
     private Dictionary<string, URL> AppInfoes = new Dictionary<string, URL>
     {
-        {
-            "Toilet", //トイレからの脱出
-            new URL
-            {
-                iOS = "https://apps.apple.com/jp/app/%E3%83%88%E3%82%A4%E3%83%AC%E3%81%8B%E3%82%89%E3%81%AE%E8%84%B1%E5%87%BA/id1620184427",
-                Android = "https://play.google.com/store/apps/details?id=com.Harekore.Escape01_toilet"
+      {
+          "Byoshitsu", //病室
+          new URL
+          {
+              iOS = "https://apps.apple.com/jp/app/%E7%97%85%E5%AE%A4%E3%81%8B%E3%82%89%E3%81%AE%E8%84%B1%E5%87%BA/id1635914051",
+              Android = "https://play.google.com/store/apps/details?id=com.Harekore.Byoshitsu"
 ,            }
-        },
+      },
+      {
+          "Karaoke", //カラオケ
+          new URL
+          {
+              iOS = "https://apps.apple.com/jp/app/%E3%82%AB%E3%83%A9%E3%82%AA%E3%82%B1%E3%83%AB%E3%83%BC%E3%83%A0%E3%81%8B%E3%82%89%E3%81%AE%E8%84%B1%E5%87%BA/id1629906364",
+              Android = "https://play.google.com/store/apps/details?id=com.Harekore.Karaoke"
+,            }
+      },
+      {
+          "Toilet", //トイレからの脱出
+          new URL
+          {
+              iOS = "https://apps.apple.com/jp/app/%E3%83%88%E3%82%A4%E3%83%AC%E3%81%8B%E3%82%89%E3%81%AE%E8%84%B1%E5%87%BA/id1620184427",
+              Android = "https://play.google.com/store/apps/details?id=com.Harekore.Escape01_toilet"
+,            }
+      },
     };
     
     // Start is called before the first frame update
     void Start()
     {
+        //病室
+
+        //トイレ
+        BtnByoshitsu.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            OnTapApp("Byoshitsu");
+        });
         //トイレ
         BtnToilet.GetComponent<Button>().onClick.AddListener(() =>
         {
